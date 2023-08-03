f13377cbe30f1bb084c2c2e8aae22cfd76057cff
julius <juliush.dev@gmail.com>
2023-08-03 19:02:40 +0200

    Compose the main widgets tree

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
-import 'package:aphasia/image_association_training_models.dart';
-import 'package:aphasia/training_session_models.dart';
+import 'package:aphasia/domain/models/quiz_training.dart';
+import 'package:aphasia/service_locator.dart';
+import 'package:aphasia/view/quiz_training_widgets.dart';
+import 'package:aphasia/view/shared_widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 void main() {
+  setup();
   runApp(const Aphasia());
 }
 
@@ -11,166 +13,137 @@ class Aphasia extends StatelessWidget {
   const Aphasia({super.key});
   @override
   Widget build(BuildContext context) {
+    const Color primaryColor = Colors.white;
+    // const Color backgroundColor = Color.fromARGB(255, 110, 228, 199);
+    const Color backgroundColor = Colors.greenAccent;
+    const ColorScheme customColorScheme = ColorScheme(
+      primary: primaryColor,
+      secondary: Colors.deepPurple,
+      surface: Colors.white,
+      background: backgroundColor,
+      error: Colors.red,
+      onPrimary: Colors.black,
+      onSecondary: Colors.white,
+      onSurface: Colors.black,
+      onBackground: Colors.black,
+      onError: Colors.white,
+      brightness: Brightness.light,
+    );
+    const customTextTheme = TextTheme(
+      displayLarge: TextStyle(
+          fontSize: 72, fontWeight: FontWeight.bold, color: Colors.black),
+      displayMedium: TextStyle(
+          fontSize: 36, fontWeight: FontWeight.bold, color: Colors.black),
+      displaySmall: TextStyle(
+          fontSize: 20, fontWeight: FontWeight.w600, color: Colors.black),
+      headlineLarge: TextStyle(
+          fontSize: 30, fontWeight: FontWeight.w500, color: Colors.black),
+      headlineMedium: TextStyle(
+          fontSize: 25, fontWeight: FontWeight.w500, color: Colors.black),
+      headlineSmall: TextStyle(
+          fontSize: 16, fontWeight: FontWeight.w500, color: Colors.black),
+      titleLarge: TextStyle(
+          fontSize: 24, fontWeight: FontWeight.w500, color: Colors.black),
+      titleMedium: TextStyle(
+          fontSize: 20, fontWeight: FontWeight.w500, color: Colors.black),
+      titleSmall: TextStyle(
+          fontSize: 18, fontWeight: FontWeight.w500, color: Colors.black),
+      bodyLarge: TextStyle(fontSize: 20, color: Colors.black),
+      bodyMedium: TextStyle(fontSize: 18, color: Colors.black),
+      bodySmall: TextStyle(fontSize: 16, color: Colors.grey),
+      labelLarge: TextStyle(
+          fontSize: 18, fontWeight: FontWeight.w600, color: Colors.white),
+      labelSmall: TextStyle(
+          fontSize: 14, fontWeight: FontWeight.w500, color: Colors.black),
+    );
+
     return MaterialApp(
       title: 'Aphasia',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: customColorScheme,
+        textTheme: customTextTheme,
         useMaterial3: true,
       ),
       home: Scaffold(
         appBar: AppBar(
-          leading: const Icon(Icons.menu),
+          title: const Text("Choose a method of training"),
         ),
-        body: HomePage(),
+        body: const HomePage(),
       ),
     );
   }
 }
 
 class HomePage extends StatelessWidget {
-  HomePage({super.key});
-
-  final availableTrainingSessions = <TrainingSession>[
-    ImageWordAssociationSession(),
-  ];
-
-  Widget buildTrainingSessionOverview({
-    required BuildContext context,
-    required TrainingSession trainingSession,
-  }) {
-    return ListTile(
-      contentPadding: const EdgeInsets.only(bottom: 15),
-      title: Center(
-        child: Text(
-          trainingSession.title.toUpperCase(),
-          style: const TextStyle(fontSize: 30),
-        ),
-      ),
-      dense: true,
-      tileColor: Colors.amber,
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => TrainingSessionDataSetCategoriesWidget(
-              trainingSession: trainingSession,
-            ),
-          ),
-        );
-      },
-    );
-  }
-
+  const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
-    Widget page = GridView.count(
-      crossAxisCount: 3,
-      padding: const EdgeInsets.all(20),
-      children: availableTrainingSessions
-          .map(
-            (trainingSession) => buildTrainingSessionOverview(
-              context: context,
-              trainingSession: trainingSession,
-            ),
-          )
-          .toList(),
-    );
-    return page;
-  }
-}
-
-class TrainingSessionDataSetCategoriesWidget extends StatelessWidget {
-  // In the constructor, require a Todo.
-  const TrainingSessionDataSetCategoriesWidget(
-      {super.key, required this.trainingSession});
-
-  // Declare a field that holds the Todo.
-  final TrainingSession trainingSession;
-
-  Widget buildSubcategoryOverview({
-    required BuildContext context,
-    required String category,
-    required TrainingSession trainingSession,
-  }) {
-    return ListTile(
-      textColor: Colors.white,
-      contentPadding: const EdgeInsets.all(10),
-      title: Center(
-        child: Text(
-          category.toUpperCase(),
-          style: const TextStyle(fontSize: 30),
-        ),
-      ),
-      dense: true,
-      tileColor: Colors.blue,
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => RunningTrainingSession(
-              trainingSession: trainingSession,
-              targetDataCategory: category,
-            ),
-          ),
-        );
-      },
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // Use the Todo to create the UI.
-    final trainingDataSetCategories = trainingSession.datasetCategories();
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(trainingSession.title.toUpperCase()),
-        ),
-        body: ListView.separated(
-          padding: const EdgeInsets.all(8),
-          itemCount: trainingDataSetCategories.length,
-          itemBuilder: (BuildContext context, int index) {
-            return buildSubcategoryOverview(
-              context: context,
-              category: trainingDataSetCategories[index],
-              trainingSession: trainingSession,
+    return FutureBuilder(
+        future: serviceLocator.allReady(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.hasData) {
+            return Builder(
+              builder: (context) {
+                final quizTrainingDataset =
+                    serviceLocator<QuizTrainingDataset>();
+                return ListView(
+                  children: [
+                    TrainingDatasetOverviewWidget(
+                        trainingDataset: quizTrainingDataset,
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) {
+                              return QuizTrainingDatasetCategoryPicker(
+                                trainingDatasetCategories:
+                                    quizTrainingDataset.categories.cast(),
+                              );
+                            }),
+                          );
+                        })
+                  ],
+                );
+              },
             );
-          },
-          separatorBuilder: (BuildContext context, int index) =>
-              const Divider(),
-        ));
-  }
-}
-
-class RunningTrainingSession extends StatelessWidget {
-  // In the constructor, require a Todo.
-  const RunningTrainingSession({
-    required this.trainingSession,
-    required this.targetDataCategory,
-    super.key,
-  });
-
-  // Declare a field that holds the Todo.
-  final TrainingSession trainingSession;
-  final String targetDataCategory;
-
-  @override
-  Widget build(BuildContext context) {
-    // Use the Todo to create the UI.
-    trainingSession.resetLastTargetDataIndex();
-    return ChangeNotifierProvider.value(
-      value: trainingSession,
-      builder: (context, child) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Text(
-                '${targetDataCategory.toUpperCase()} ${trainingSession.title.toUpperCase()} ${'Session'.toUpperCase()}'),
-          ),
-          body: trainingSession.startSession(
-            context: context,
-            targetDataCategory: targetDataCategory,
-          ),
-        );
-      },
-    );
+          } else if (snapshot.hasError) {
+            throw (snapshot.stackTrace!, snapshot.error!);
+            // return Center(
+            //   child: Column(
+            //     mainAxisAlignment: MainAxisAlignment.center,
+            //     crossAxisAlignment: CrossAxisAlignment.center,
+            //     children: <Widget>[
+            //       Icon(
+            //         Icons.error_outline,
+            //         color: Theme.of(context).colorScheme.error,
+            //         size: 60,
+            //       ),
+            //       Padding(
+            //         padding: const EdgeInsets.only(top: 16),
+            //         child: Text('Error: ${snapshot.error}'),
+            //       ),
+            //     ],
+            //   ),
+            // );
+          } else {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  const SizedBox(
+                    width: 60,
+                    height: 60,
+                    child: CircularProgressIndicator(),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16),
+                    child: Text('Loading Training dataset...',
+                        style: Theme.of(context).textTheme.bodyLarge),
+                  ),
+                ],
+              ),
+            );
+          }
+        });
   }
 }
