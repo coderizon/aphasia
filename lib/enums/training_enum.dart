2b3bb1fccecb395f60510195c798c39d08ee0f78
julius <juliush.dev@gmail.com>
2023-08-03 18:57:44 +0200

    Define training related enumarations

---
+++ lib/enums/training_enum.dart
@@ -0,0 +1,18 @@
+enum TrainingSessionState {
+  listening,
+  stopped,
+  neverStarted;
+}
+
+enum TrainingSessionException {
+  listening,
+  noCurrentData,
+  notListening,
+  stopped,
+  neverStarted;
+}
+
+enum ImageSource {
+  www,
+  local;
+}
