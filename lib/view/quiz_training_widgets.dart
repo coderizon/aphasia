e913ec2c689d98f1a21f738a0950099dcd830d4c
julius <juliush.dev@gmail.com>
2023-08-07 19:48:36 +0200

    Extend and Reorganize state dispatching

--- lib/view/quiz_training_widgets.dart
+++ lib/view/quiz_training_widgets.dart
@@ -3,99 +3,37 @@ import 'dart:async';
 import 'package:aphasia/domain/models/quiz_training.dart';
 import 'package:aphasia/domain/models/training.dart';
 import 'package:aphasia/extensions.dart';
-import 'package:aphasia/service_locator.dart';
 import 'package:aphasia/view/shared_widgets.dart';
 import 'package:aphasia/widgets_related_enum.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class QuizTrainingDatasetCategoryPicker extends StatelessWidget {
-  final List<QuizTrainingDatasetCategory> trainingDatasetCategories;
-  const QuizTrainingDatasetCategoryPicker({
+class QuizTrainingSessionWidget extends StatelessWidget {
+  const QuizTrainingSessionWidget({
     super.key,
-    required this.trainingDatasetCategories,
   });
 
   @override
   Widget build(BuildContext context) {
-    // final theme = Theme.of(context);
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Choose a category to train on'),
+        title: const Text('Quiz session'),
       ),
-      body: ListView(
-        children: trainingDatasetCategories
-            .map(
-              (category) => TrainingDatasetCategoryOverviewWidget(
-                overview: category.overview,
-                onTap: () {
-                  initializeQuizSession(
-                    context,
-                    category,
-                  );
-                },
-              ),
-            )
-            .toList(),
+      body: Consumer<TrainingSession>(
+        builder: (_, trainingSession, __) {
+          return trainingSession.currentData == null
+              ? QuizTrainingStatisticsWidget(
+                  trainingSession: trainingSession,
+                )
+              : const QuizWidget();
+        },
       ),
     );
-  }
-
-  void initializeQuizSession(
-    BuildContext context,
-    TrainingDatasetCategory trainingDatasetCategory,
-  ) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) {
-        final trainingSession = serviceLocator<QuizTrainingSession>(
-            param1: trainingDatasetCategory);
-        return Builder(
-          builder: (context) {
-            return Scaffold(
-              appBar: AppBar(
-                title: const Text("New training session"),
-              ),
-              body: QuizTrainingSessionWidget(
-                trainingSession: trainingSession,
-              ),
-            );
-          },
-        );
-      }),
-    );
-  }
-}
-
-class QuizTrainingSessionWidget extends TrainingSessionWidget {
-  QuizTrainingSessionWidget({
-    super.key,
-    required super.trainingSession,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider.value(
-        value: trainingSession,
-        builder: (context, child) {
-          final QuizTrainingSession trainingSession =
-              context.watch<TrainingSession>() as QuizTrainingSession;
-
-          if (trainingSession.currentData == null) {
-            return QuizTrainingStatisticsWidget(
-              trainingSession: trainingSession,
-            );
-          } else {
-            return QuizWidget(
-              trainingSession: trainingSession,
-            );
-          }
-        });
   }
 }
 
 class QuizTrainingStatisticsWidget extends StatelessWidget {
-  final QuizTrainingSession trainingSession;
+  final TrainingSession trainingSession;
   const QuizTrainingStatisticsWidget(
       {super.key, required this.trainingSession});
 
@@ -115,114 +53,126 @@ class QuizTrainingStatisticsWidget extends StatelessWidget {
 }
 
 class UserGratificationActivator extends ChangeNotifier {
-  var _userGotItRight = false;
+  bool? _userGotItRight;
   get userGotItRight => _userGotItRight;
   void gratifyUser() {
     _userGotItRight = true;
     notifyListeners();
   }
 
-  void destroyGratification() {
+  void userGotItWrong() {
     _userGotItRight = false;
+    notifyListeners();
+  }
+
+  void destroyGratification() {
+    _userGotItRight = null;
     // notifyListeners();
   }
 }
 
 class QuizWidget extends StatelessWidget {
-  final QuizTrainingSession trainingSession;
-  const QuizWidget({super.key, required this.trainingSession});
+  const QuizWidget({super.key});
+  List<String> choices(
+      List<QuizTrainingData> dataset, QuizTrainingData answer) {
+    final falseChoices = dataset
+        .where((element) => element.id != answer.id)
+        .toList()
+        .sublist(0, 2);
+    final List<QuizTrainingData> choices = List.from(falseChoices);
+    choices.add(answer);
+    return choices.map((option) => option.answer as String).toList();
+  }
 
   @override
   Widget build(BuildContext context) {
+    final TrainingSession trainingSession = context.watch<TrainingSession>();
     QuizTrainingData? trainingData =
         trainingSession.currentData as QuizTrainingData?;
     return ChangeNotifierProvider(
         create: (context) => UserGratificationActivator(),
         builder: (context, child) {
-          final userGratificationActivator =
-              context.watch<UserGratificationActivator>();
-          if (!userGratificationActivator.userGotItRight) {
-            return Padding(
-              padding: const EdgeInsets.all(20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10),
+          return Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 10),
+                    child: Consumer<UserGratificationActivator>(
+                      builder: (context, userGratificationActivator, child) {
+                        return userGratificationActivator._userGotItRight ==
+                                    null ||
+                                userGratificationActivator._userGotItRight ==
+                                    false
+                            ? Text(
+                                "${trainingSession.currentInstruction} ${userGratificationActivator._userGotItRight == false ? '😲' : '😇'}",
+                                style:
+                                    Theme.of(context).textTheme.headlineLarge,
+                              )
+                            : CongratulationWidget(
+                                answer: trainingData!,
+                                onCountdownOver: trainingSession.nextData,
+                                misses: trainingSession.answerStats.misses,
+                                successes:
+                                    trainingSession.answerStats.successes,
+                              );
+                      },
+                    )),
+                const SizedBox(
+                  height: 25,
+                ),
+                Card(
+                  elevation: 20,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    side: const BorderSide(color: Colors.greenAccent, width: 4),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.network(
+                      trainingData!.image,
+                      height: 250,
+                      fit: BoxFit.cover,
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 10),
-                      child: Text(
-                        "${trainingSession.currentInstruction} 🧐",
-                        style: Theme.of(context).textTheme.headlineLarge,
-                      ),
-                    ),
                   ),
-                  const SizedBox(
-                    height: 25,
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                QuizUserInputListenerPad(
+                  trainingData: trainingData,
+                  choices: choices(
+                    trainingSession.trainingDataset as List<QuizTrainingData>,
+                    trainingData,
                   ),
-                  Card(
-                    elevation: 20,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                      side:
-                          const BorderSide(color: Colors.greenAccent, width: 4),
-                    ),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Image.network(
-                        trainingData!.image,
-                        height: 250,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 25,
-                  ),
-                  QuizUserInputListenerPad(
-                    trainingData: trainingData,
-                    datasetShuffler: trainingSession.shuffleTrainingDataset,
-                    inputValidator: trainingSession.validateUserAnswer,
-                    nextData: trainingSession.nextData,
-                    userGratificationActivator: userGratificationActivator,
-                  ),
-                ],
-              ),
-            );
-          } else {
-            return CongratulationWidget(
-              answer: trainingData!,
-              onCountdownOver: trainingSession.nextData,
-              misses: trainingSession.answerStats.misses,
-              successes: trainingSession.answerStats.successes,
-            );
-          }
+                  inputValidator: trainingSession.validateUserAnswer,
+                  nextData: trainingSession.nextData,
+                ),
+              ],
+            ),
+          );
         });
   }
 }
 
 typedef DatasetShuffler = List<String> Function(int);
 typedef InputValidator = bool Function(String);
-typedef DatasetIterator = TrainingData? Function();
+typedef DatasetIterator = void Function();
 
 class QuizUserInputListenerPad extends StatefulWidget {
   final QuizTrainingData trainingData;
-  final DatasetShuffler datasetShuffler;
+  final List<String> choices;
   final InputValidator inputValidator;
   final DatasetIterator nextData;
-  final UserGratificationActivator userGratificationActivator;
   const QuizUserInputListenerPad({
     super.key,
-    required this.datasetShuffler,
     required this.inputValidator,
+    required this.choices,
     required this.nextData,
     required this.trainingData,
-    required this.userGratificationActivator,
   });
 
   @override
@@ -233,6 +183,21 @@ class QuizUserInputListenerPad extends StatefulWidget {
 class _QuizUserInputListenerPadState extends State<QuizUserInputListenerPad> {
   var _micState = MicState.disabled;
   var _transcript = TranscriptState.ready.text;
+  List<String>? _choices;
+
+  @override
+  void initState() {
+    super.initState();
+    _choices = widget.choices;
+    _choices!.shuffle();
+  }
+
+  void shuffleChoices() {
+    setState(() {
+      widget.choices.shuffle();
+      _choices = widget.choices;
+    });
+  }
 
   void notifyMicState(MicState micState) {
     setState(() {
@@ -241,7 +206,7 @@ class _QuizUserInputListenerPadState extends State<QuizUserInputListenerPad> {
           widget.inputValidator(_transcript.toLowerCase())) {
         _micState = MicState.disabled;
         _transcript = TranscriptState.ready.text;
-        widget.userGratificationActivator.gratifyUser();
+        context.read<UserGratificationActivator>().gratifyUser();
       }
     });
   }
@@ -255,7 +220,8 @@ class _QuizUserInputListenerPadState extends State<QuizUserInputListenerPad> {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    final shuffledPossibilities = widget.datasetShuffler(2);
+    final userGotitRight =
+        context.watch<UserGratificationActivator>()._userGotItRight;
     if (_micState == MicState.done) {
       _micState = MicState.disabled;
     }
@@ -265,26 +231,43 @@ class _QuizUserInputListenerPadState extends State<QuizUserInputListenerPad> {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           if (_micState == MicState.disabled)
-            ...shuffledPossibilities
-                .map((e) => SizedBox(
-                      height: 60,
-                      child: ElevatedButton(
-                        child: Text(e.capitalize(),
-                            style: theme.textTheme.bodyLarge!.copyWith(
-                                color: theme.colorScheme.onPrimary,
-                                fontSize: 25)),
-                        onPressed: () async {
-                          if (widget.inputValidator(e)) {
-                            widget.userGratificationActivator.gratifyUser();
-                          } else {
-                            setState(() {
-                              // trigger shuffled
-                            });
-                          }
-                        },
-                      ),
-                    ))
-                .toList()
+            ...widget.choices.map((option) {
+              final optionIsAnswer = option == widget.trainingData.answer;
+              return SizedBox(
+                height: 60,
+                child: ElevatedButton(
+                  style: userGotitRight == true
+                      ? (ElevatedButton.styleFrom(
+                          backgroundColor: optionIsAnswer
+                              ? Colors.white
+                              : Colors.greenAccent,
+                        ))
+                      : null,
+                  child: Text(
+                    '${option.capitalize()}${userGotitRight == true && optionIsAnswer ? " ✅🥳" : ""}',
+                    style: theme.textTheme.bodyLarge!.copyWith(
+                      color: userGotitRight == true && !optionIsAnswer
+                          ? Colors.black38
+                          : theme.colorScheme.onPrimary,
+                      fontSize: 25,
+                      decoration: userGotitRight == true && !optionIsAnswer
+                          ? TextDecoration.lineThrough
+                          : null,
+                    ),
+                  ),
+                  onPressed: () async {
+                    if (widget.inputValidator(option)) {
+                      context.read<UserGratificationActivator>().gratifyUser();
+                    } else {
+                      context
+                          .read<UserGratificationActivator>()
+                          .userGotItWrong();
+                      shuffleChoices();
+                    }
+                  },
+                ),
+              );
+            }).toList()
           else
             Expanded(
               child: Container(
@@ -309,7 +292,7 @@ class _QuizUserInputListenerPadState extends State<QuizUserInputListenerPad> {
   }
 }
 
-typedef OnCountdownOver = TrainingData? Function();
+typedef OnCountdownOver = void Function();
 
 class CongratulationWidget extends StatefulWidget {
   final TrainingData answer;
@@ -329,7 +312,7 @@ class CongratulationWidget extends StatefulWidget {
 }
 
 class _CongratulationWidgetState extends State<CongratulationWidget> {
-  var _countdownBeforeNextChallenge = 8;
+  var _countdownBeforeNextChallenge = 3;
   Timer? _timer;
   void decrementTimeOutBeforeNextChallenge() {
     setState(() {
@@ -358,95 +341,25 @@ class _CongratulationWidgetState extends State<CongratulationWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              // borderRadius: BorderRadius.circular(10),
+    return _timer != null && _timer!.isActive
+        ? Center(
+            child: Text(
+              'Next question in $_countdownBeforeNextChallenge',
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineLarge!
+                  .copyWith(color: Colors.black45),
             ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
-              child: Text(
-                "✅ 😎 Yeah! Scroll to read more 💚!",
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
+          )
+        : Center(
+            child: Text(
+              'Next question in 3',
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineLarge!
+                  .copyWith(color: Colors.black45),
             ),
-          ),
-          Image.network(
-            (widget.answer as QuizTrainingData).image,
-            height: 250,
-            fit: BoxFit.cover,
-          ),
-          Expanded(
-            child: NotificationListener<ScrollNotification>(
-              onNotification: (scrollNotification) {
-                if (scrollNotification is ScrollUpdateNotification) {
-                  if (_timer != null) {
-                    setState(() {
-                      _timer!.cancel();
-                    });
-                  }
-                }
-                return false;
-              },
-              child: SingleChildScrollView(
-                child: Container(
-                  padding: const EdgeInsets.all(20),
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                  ),
-                  child: Text(
-                    (widget.answer as QuizTrainingData).funFact,
-                    style: Theme.of(context).textTheme.bodyLarge,
-                  ),
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          if (_timer != null && _timer!.isActive)
-            Center(
-              child: Text(
-                'Next question in $_countdownBeforeNextChallenge seconds',
-              ),
-            )
-          else
-            const Center(
-              child: Text(
-                'No rush! Take your time!🥰',
-              ),
-            ),
-          const SizedBox(
-            height: 20,
-          ),
-          SizedBox(
-            height: 60,
-            child: ElevatedButton(
-              onPressed: () {
-                _timer!.cancel();
-                context
-                    .read<UserGratificationActivator>()
-                    .destroyGratification();
-                widget.onCountdownOver();
-              },
-              child: Text(
-                'Continue quiz',
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyLarge!
-                    .copyWith(color: Colors.black),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
+          );
   }
 }
 
